ae2da30081d2fff57526fc93cb78edf4d5122e32
Dimitris Liaropoulos <dimliarop@gmail.com>
2024-09-12 16:30:09 +0300

    skeleton to leavesmetric

--- components/Home/Metrics.vue
+++ components/Home/Metrics.vue
@@ -1,30 +1,38 @@
 <template>
-    <div class="bg-white border rounded-lg shadow-md p-4">
+    <div :class="{ 'bg-gray-200': loading, 'bg-white': !loading }" class="border rounded-lg shadow-md p-4">
         <div class="grid grid-cols-2 gap-4">
             <div>
-                <h5 class="text-lg font-semibold mb-4">{{ leave.leave_type_name }}</h5>
-                <div>
-                    <span class="font-bold">Σύνολο: </span>
-                    <span class="font-light">{{ leave.entitled_days }}</span>
-                </div>
-                <div>
-                    <span class="font-bold">Υπόλοιπο: </span>
-                    <span class="font-light">{{ leave.remaining_days }}</span>
-                </div>
+                <template v-if="loading">
+                </template>
+                <template v-else>
+                    <h5 class="text-lg font-semibold mb-4">{{ leave.leave_type_name }}</h5>
+                    <div>
+                        <span class="font-bold">Σύνολο: </span>
+                        <span class="font-light">{{ leave.entitled_days }}</span>
+                    </div>
+                    <div>
+                        <span class="font-bold">Υπόλοιπο: </span>
+                        <span class="font-light">{{ leave.remaining_days }}</span>
+                    </div>
+                </template>
             </div>
             <div class="chart-container">
-                <ClientOnly>
-                    <ApexCharts :options="chartOptions" :series="chartSeries" type="radialBar" />
-                </ClientOnly>
+                <template v-if="loading">
+                    <div class="skeleton-chart h-[70px]"></div>
+                </template>
+                <template v-else>
+                    <ClientOnly>
+                        <ApexCharts :options="chartOptions" :series="chartSeries" type="radialBar" />
+                    </ClientOnly>
+                </template>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
-import { ref, computed, watch } from 'vue';
+import { ref, computed, watch, onMounted } from 'vue';
 import ApexCharts from 'vue3-apexcharts';
-import { defineProps } from 'vue';
 
 const props = defineProps({
     leave: {
@@ -33,7 +41,8 @@ const props = defineProps({
     }
 });
 
-// Calculate percentage of remaining days based on entitled days
+const loading = ref(true);
+
 const percentageRemaining = computed(() => {
     const { entitled_days, remaining_days } = props.leave;
     return entitled_days > 0 ? (remaining_days / entitled_days) * 100 : 0;
@@ -70,10 +79,13 @@ const chartSeries = ref([0]); // Initialize with 0
 watch(() => props.leave.remaining_days, (newValue) => {
     chartSeries.value = [percentageRemaining.value]; // Update the chart series data based on the calculated percentage
 }, { immediate: true }); // Run the watch function immediately on component mount
+
+onMounted(() => {
+    // Simulate loading time
+    setTimeout(() => {
+        loading.value = false;
+    }, 2000); // Simulate a 2-second loading time
+});
 </script>
 
-<script>
-export default {
-    name: "Metrics",
-};
-</script>
+<style scoped></style>
