3304b38a7c1f673d126aa81702e145c2481d3a9e
Dimitris Liaropoulos <dimliarop@gmail.com>
2024-09-12 14:40:46 +0300

    Leaves metric

--- components/Home/LeavesMetric.vue
+++ components/Home/LeavesMetric.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="container mx-auto">
         <h3 class="py-4 font-semibold text-lg">Υπόλοιπα άδειας</h3>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-            <Metrics />
-            <Metrics />
-            <Metrics />
+        <div v-if="leavesData.length === 0" class="text-center py-4 text-gray-600">
+            Δεν υπάρχουν διαθέσιμες άδειες
+        </div>
+        <div v-else class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+            <!-- Render Metrics component for each item in leavesData -->
+            <Metrics v-for="(leave, index) in leavesData" :key="index" :leave="leave" />
             <div class="flex flex-row justify-center items-center leave-actions space-x-4">
                 <NewLeave />
                 <CancelLeave />
@@ -17,4 +19,12 @@
 import Metrics from '~/components/Home/Metrics.vue'
 import NewLeave from '~/components/Home/NewLeave.vue'
 import CancelLeave from '~/components/Home/CancelLeave.vue'
+import { computed } from 'vue';
+import { useCentralStore } from '@/stores/centralStore';
+
+const centralStore = useCentralStore();
+const leavesStore = centralStore.leavesStore;
+
+// Use computed to get leavesData
+const leavesData = computed(() => leavesStore.leavesData.leavesAvailableDays);
 </script>
--- components/Home/Metrics.vue
+++ components/Home/Metrics.vue
@@ -2,26 +2,36 @@
     <div class="bg-white border rounded-lg shadow-md p-4">
         <div class="grid grid-cols-2 gap-4">
             <div>
-                <h5 class="text-lg font-semibold mb-4">Πληρωμένη άδεια</h5>
+                <h5 class="text-lg font-semibold mb-4">{{ leave.leave_type_name }}</h5>
                 <div>
-                    <span class="font-bold">Σύνολο: </span> <span class="font-light">22</span>
+                    <span class="font-bold">Σύνολο: </span>
+                    <span class="font-light">{{ leave.entitled_days }}</span>
                 </div>
                 <div>
-                    <span class="font-bold">Σύνολο: </span> <span class="font-light">22</span>
+                    <span class="font-bold">Υπόλοιπο: </span>
+                    <span class="font-light">{{ leave.remaining_days }}</span>
                 </div>
             </div>
             <div class="chart-container">
                 <ClientOnly>
-                    <ApexCharts :options="chartOptions" :series="chartSeries" type="radialBar"/>
+                    <ApexCharts :options="chartOptions" :series="chartSeries" type="radialBar" />
                 </ClientOnly>
             </div>
         </div>
     </div>
-
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from 'vue';
+import ApexCharts from 'vue3-apexcharts';
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    leave: {
+        type: Object,
+        required: true
+    }
+});
 
 const chartOptions = ref({
     chart: {
@@ -35,12 +45,12 @@ const chartOptions = ref({
             dataLabels: {
                 enabled: true,
                 name: {
-                    show: true, 
-                    formatter: (val) => 'Υπόλοιπο', 
+                    show: true,
+                    formatter: () => 'Υπόλοιπο',
                 },
                 value: {
-                    show: true, 
-                    formatter: (val) => '12',
+                    show: true,
+                    formatter: (val) => `${val}`, // Update formatter to use dynamic value
                 },
             },
         },
@@ -48,7 +58,12 @@ const chartOptions = ref({
     colors: ['#3342ff'], // Customize colors
 });
 
-const chartSeries = ref([80]); // Series data
+const chartSeries = ref([0]); // Initialize with 0
+
+// Watch for changes in props to update chart data
+watch(() => props.leave.remaining_days, (newValue) => {
+    chartSeries.value = [newValue]; // Update the chart series data based on the remaining days
+}, { immediate: true }); // Run the watch function immediately on component mount
 </script>
 
 <script>
