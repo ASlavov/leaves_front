2ac0dd1904dde492241be6ffdf51b58cbf3a08c7
Dimitris Liaropoulos <dimliarop@gmail.com>
2024-09-16 16:17:36 +0300

    cancel leave dynamic api

--- components/Home/CancelLeave.vue
+++ components/Home/CancelLeave.vue
@@ -21,7 +21,7 @@
                         <path d="m6 6 12 12"></path>
                     </svg>
                 </button>
-                <div class="flex  items-center py-3 px-4">
+                <div class="flex items-center py-3 px-4">
                     <h3 id="hs-scale-animation-modal-label"
                         class="font-bold text-gray-800 dark:text-white max-w-[300px] mx-auto text-lg">
                         Ακύρωση άδειας
@@ -31,72 +31,31 @@
                 <div class="p-4 overflow-y-auto">
                     <div class="new-leave-form py-10">
 
-                        <form class="cancel-leave">
+                        <form class="cancel-leave" @submit.prevent="handleSubmit">
 
                             <div class="max-h-60 overflow-y-auto">
-
-                                <label for="hs-radioradio-on-right"
-                                    class="my-2 flex items-center justify-between p-3 w-full bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
-                                    <input type="radio" name="hs-radio-on-right"
-                                        class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                                        id="hs-radio-on-right">
-                                    <span class="text-xs text-gray-400 mx-auto">27/12 - 29/12<br><span
-                                            class="text-sm text-black font-semibold">3 ημέρες</span></span>
-                                    <span
-                                        class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">Πληρωμένη
-                                        άδεια</span>
-                                    <span class="text-sm ms-auto text-green-700 font-semibold">Αποδεκτή</span>
-                                </label>
-
-                                <label for="hs-radioradio-on-right"
-                                    class="my-2 flex items-center justify-between p-3 w-full bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
-                                    <input type="radio" name="hs-radio-on-right"
-                                        class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                                        id="hs-radio-on-right">
-                                    <span class="text-xs text-gray-400 mx-auto">27/12 - 29/12<br><span
-                                            class="text-sm text-black font-semibold">3 ημέρες</span></span>
-                                    <span
-                                        class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">Πληρωμένη
-                                        άδεια</span>
-                                    <span class="text-sm ms-auto text-green-700 font-semibold">Αποδεκτή</span>
-                                </label>
-
-                                <label for="hs-radioradio-on-right"
-                                    class="my-2 flex items-center justify-between p-3 w-full bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
-                                    <input type="radio" name="hs-radio-on-right"
-                                        class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                                        id="hs-radio-on-right">
-                                    <span class="text-xs text-gray-400 mx-auto">27/12 - 29/12<br><span
-                                            class="text-sm text-black font-semibold">3 ημέρες</span></span>
-                                    <span
-                                        class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">Πληρωμένη
-                                        άδεια</span>
-                                    <span class="text-sm ms-auto text-green-700 font-semibold">Αποδεκτή</span>
-                                </label>
-
-                                <label for="hs-radioradio-on-right"
-                                    class="my-2 flex items-center justify-between p-3 w-full bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
-                                    <input type="radio" name="hs-radio-on-right"
-                                        class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
-                                        id="hs-radio-on-right">
-                                    <span class="text-xs text-gray-400 mx-auto">27/12 - 29/12<br><span
-                                            class="text-sm text-black font-semibold">3 ημέρες</span></span>
-                                    <span
-                                        class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">Πληρωμένη
-                                        άδεια</span>
-                                    <span class="text-sm ms-auto text-green-700 font-semibold">Αποδεκτή</span>
-                                </label>
-
+                                <!-- Loop through filteredLeavesData and create a radio button for each leave -->
+                                <div v-for="leave in filteredLeavesData" :key="leave.id">
+                                    <label :for="'leave-' + leave.id"
+                                        class="my-2 flex items-center justify-between p-3 w-full bg-white border border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400">
+                                        <input type="radio" name="leave" :id="'leave-' + leave.id"
+                                            class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
+                                            :value="leave" v-model="selectedLeave">
+                                        <span class="text-xs text-gray-400 mx-auto">{{ leave.start_date }} - {{ leave.end_date }}<br>
+                                            <span class="text-sm text-black font-semibold">{{ calculateDays(leave.start_date, leave.end_date) }} ημέρες</span>
+                                        </span>
+                                        <span class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">{{ getLeaveTypeName(leave.leave_type_id) }}</span>
+                                        <span class="text-sm ms-auto text-green-700 font-semibold">{{ leave.status === 'pending' ? 'Αναμονή' : 'Αποδεκτή' }}</span>
+                                    </label>
+                                </div>
                             </div>
-
 
                             <div class="space-y-3 pt-5">
                                 <label for="textarea" class="block text-sm font-medium text-gray-700 py-3">Σχόλια
                                     (προαιρετικό)</label>
                                 <textarea class="py-3 px-4 block w-full border-gray-200 border text-sm rounded-lg"
-                                    rows="3" placeholder="Τα σχόλια σας"></textarea>
+                                    rows="3" placeholder="Τα σχόλια σας" v-model="comment"></textarea>
                             </div>
-
 
                             <div class="py-5">
                                 <button type="submit"
@@ -112,7 +71,6 @@
             </div>
         </div>
     </div>
-
 </template>
 
 <script>
@@ -120,17 +78,82 @@ import { ref, computed } from 'vue';
 import { useCentralStore } from '@/stores/centralStore.js';
 
 export default {
-    name: ' CancelLeave',
+    name: 'CancelLeave',
     setup() {
         const centralStore = useCentralStore();
         const leavesStore = centralStore.leavesStore;
 
         // Ensure leavesData is always an array
-        const leavesData = computed(() => leavesStore.leavesData?.leavesAvailableDays || []);
-        const userStore = centralStore.userStore;
-        const user_id = computed(() => userStore.userId);
+        const leavesData = computed(() => Array.isArray(leavesStore.leavesData?.currentUser) ? leavesStore.leavesData.currentUser : []);
 
-    },
+        const leaveTypes = computed(() => leavesStore.leavesData?.leavesTypes || []);
+        
+        // Filter leaves based on status
+        const filteredLeavesData = computed(() =>
+            leavesData.value.filter(leave =>
+                leave.status === 'pending' || leave.status === 'approved'
+            )
+        );
+
+        // Selected leave and comment
+        const selectedLeave = ref(null);
+        const comment = ref('');
+
+        // Function to get leave type name based on leave_type_id
+        const getLeaveTypeName = (leaveTypeId) => {
+            const leaveType = leaveTypes.value.find(type => type.id === leaveTypeId);
+            return leaveType ? leaveType.name : 'Unknown'; // Default to 'Unknown' if not found
+        };
+
+        // Function to calculate the number of days between start_date and end_date
+        const calculateDays = (startDate, endDate) => {
+            const start = new Date(startDate);
+            const end = new Date(endDate);
+            const diffTime = Math.abs(end - start);
+            const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) + 1; // +1 to include the end date
+            return diffDays;
+        };
+
+        // Handle form submission
+        const handleSubmit = async () => {
+            if (selectedLeave.value) {
+                console.log('Selected Leave:', selectedLeave.value);
+                console.log('Comment:', comment.value);
+                try {
+                    await leavesStore.cancelLeave(
+                        centralStore.userStore.userId, // Pass userId
+                        selectedLeave.value.id,   
+                        'cancelled',      // Pass leaveId
+                        comment.value
+                    );
+
+                    useNuxtApp().$toast.success('Η αίτηση άδειας υποβλήθηκε επιτυχώς!', {
+                        position: "bottom-right",
+                        autoClose: 5000, // Close automatically after 5 seconds
+                    });
+
+                } catch (error) {
+                    console.error('Error submitting leave request:', error);
+                    successMessage.value = ''; // Clear the success message on error
+                    // Handle the error as needed
+                }
+            } else {
+                console.log('No leave selected');
+            }
+        };
+
+        return {
+            filteredLeavesData,
+            selectedLeave,
+            comment,
+            calculateDays,
+            getLeaveTypeName,
+            handleSubmit
+        };
+    }
 }
-
 </script>
+
+<style scoped>
+/* Add your custom styles here if needed */
+</style>
