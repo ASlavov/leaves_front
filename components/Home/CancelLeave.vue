83e6fdbe508e734bf29c0aa1e979f7806c86f2fe
Dimitris Liaropoulos <dimliarop@gmail.com>
2024-09-16 16:43:16 +0300

    fix close modal cancel leave

--- components/Home/CancelLeave.vue
+++ components/Home/CancelLeave.vue
@@ -1,18 +1,18 @@
 <template>
     <a href="#" class="py-3 px-5 inline-flex items-center gap-x-2 text-md dark:text-gray-100" aria-haspopup="dialog" aria-expanded="false"
-        aria-controls="hs-scale-animation-modal" data-hs-overlay="#cancel_leaves">
+        aria-controls="hs-scale-animation-modal" data-hs-overlay="#cancel-leaves">
         Ακύρωση Άδειας
     </a>
 
-    <div id="cancel_leaves"
+    <div id="cancel-leaves"
         class="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
         role="dialog" tabindex="-1" aria-labelledby="hs-scale-animation-modal-label">
         <div
             class="hs-overlay-animation-target hs-overlay-open:scale-100 hs-overlay-open:opacity-100 scale-95 opacity-0 ease-in-out transition-all duration-200 sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
-            <div class="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto">
+            <div class="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:text-gray-100">
                 <button type="button"
                     class="size-8 ml-auto inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
-                    aria-label="Close" data-hs-overlay="#cancel_leaves">
+                    aria-label="Close" data-hs-overlay="#cancel-leaves">
                     <span class="sr-only">Close</span>
                     <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -27,7 +27,7 @@
                         Ακύρωση άδειας
                     </h3>
                 </div>
-                <div class="mx-auto font-extralight text-gray-500">Επιλέξετε την άδεια που θέλετε να ακυρώσετε</div>
+                <div class="mx-auto font-extralight text-gray-500 dark:text-gray-100">Επιλέξετε την άδεια που θέλετε να ακυρώσετε</div>
                 <div class="p-4 overflow-y-auto">
                     <div class="new-leave-form py-10">
 
@@ -42,18 +42,18 @@
                                             class="shrink-0 ms-auto mt-0.5 border-gray-200 rounded-full text-blue-600 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800"
                                             :value="leave" v-model="selectedLeave">
                                         <span class="text-xs text-gray-400 mx-auto">{{ leave.start_date }} - {{ leave.end_date }}<br>
-                                            <span class="text-sm text-black font-semibold">{{ calculateDays(leave.start_date, leave.end_date) }} ημέρες</span>
+                                            <span class="text-sm text-black font-semibold dark:text-gray-100">{{ calculateDays(leave.start_date, leave.end_date) }} ημέρες</span>
                                         </span>
-                                        <span class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold">{{ getLeaveTypeName(leave.leave_type_id) }}</span>
-                                        <span class="text-sm ms-auto text-green-700 font-semibold">{{ leave.status === 'pending' ? 'Αναμονή' : 'Αποδεκτή' }}</span>
+                                        <span class="text-sm text-black-600 dark:text-neutral-400 mx-auto font-semibold ">{{ getLeaveTypeName(leave.leave_type_id) }}</span>
+                                        <span class="text-sm ms-auto text-green-700 font-semibold dark:text-gray-100">{{ leave.status === 'pending' ? 'Αναμονή' : 'Αποδεκτή' }}</span>
                                     </label>
                                 </div>
                             </div>
 
                             <div class="space-y-3 pt-5">
-                                <label for="textarea" class="block text-sm font-medium text-gray-700 py-3">Σχόλια
+                                <label for="textarea" class="block text-sm font-medium text-gray-700 py-3 dark:text-gray-100">Σχόλια
                                     (προαιρετικό)</label>
-                                <textarea class="py-3 px-4 block w-full border-gray-200 border text-sm rounded-lg"
+                                <textarea class="py-3 px-4 block w-full border-gray-200 border text-sm rounded-lg dark:bg-neutral-800 dark:text-gray-100"
                                     rows="3" placeholder="Τα σχόλια σας" v-model="comment"></textarea>
                             </div>
 
