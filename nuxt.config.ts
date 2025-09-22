c1b148c1417bd91f2c5ab768b1b0f9d0e64907b0
Ares Slavov <admin@ares.slavov.eu>
2025-09-22 13:44:59 +0300

    fix nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'netlify',
+  },
   compatibilityDate: '2024-04-03',
   devtools: {
-    enabled: true,
+    enabled: false,
 
     timeline: {
       enabled: true,
