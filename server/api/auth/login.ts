c3f41d074dd5fa03ceef2fa6d4b3a3ed81d367cd
Ares Slavov <admin@ares.slavov.eu>
2026-04-06 15:23:01 +0300

    more netlify changes

--- server/api/auth/login.ts
+++ server/api/auth/login.ts
@@ -29,6 +29,8 @@ export default defineEventHandler(async (event) => {
       headers: {
         'Content-Type': 'application/json',
         'X-CSRF-TOKEN': config.apiSecret,
+        'User-Agent': 
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       },
     })) as FetchResult;
 
